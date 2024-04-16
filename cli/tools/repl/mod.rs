82042fc286ce0ac15e9a1103c6690505b3a83284
maxmcd <max.t.mcdonnell@gmail.com>
2024-04-16 15:03:34 -0400

    Add Val Town to repl output

--- cli/tools/repl/mod.rs
+++ cli/tools/repl/mod.rs
@@ -239,7 +239,7 @@ pub async fn run(flags: Flags, repl_flags: ReplFlags) -> Result<i32, AnyError> {
   // Doing this manually, instead of using `log::info!` because these messages
   // are supposed to go to stdout, not stderr.
   if !cli_options.is_quiet() {
-    println!("Deno {}", crate::version::deno());
+    println!("Deno {} w/ Val Town", crate::version::deno());
     println!("exit using ctrl+d, ctrl+c, or close()");
     if repl_flags.is_default_command {
       println!(
